0365a6a499bfe5cf0c3123eaf59fc76f5549a506
inno-inho <zergling2565@gmail.com>
2026-01-02 12:27:21 +0900

    12강까지 들음

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
-import { useState } from "react";
-import { addTodo } from "./services/todoService";
+import { useEffect, useState } from "react";
+import { addTodo, deleteTodo, fetchTodos, toggleTodo } from "./services/todoService";
+import type { Todo } from "./types/todo";
 
 function App() {
 
+  const [todos, setTodos] = useState<Todo[]>([]);
   const [newTitle, setNewTitle] = useState("");
   const [adding, setAdding] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // 초기 Loading
+  useEffect(() => {
+    (
+      async () => {
+        setLoading(true);
+        try{
+          const data = await fetchTodos();
+          setTodos(data);
+        } catch(error){
+          console.error("에러가 발생했습니다. ", error);
+          alert("データを読んでる間エラーが発生しました。")
+        } finally{
+          setLoading(false);
+        }
+      }
+    )();    // 즉시 실행 함수 IIFE(Immediately Invoked Function Expression)
+  }, []);
+
+  // 완료 체크 토글
+  const handleToggle = async (id: string, completed: boolean) => {
+    try {
+      await toggleTodo(id, completed);
+      setTodos((prev) => prev.map(
+        (todo) => todo.id === id ? {...todo, completed} : todo
+      ));
+    } catch(error){
+      console.error("업데이트 중 오류 발생", error);
+      alert("Update中、エラーが発生しました。")
+    }
+  }
 
   // Todo 추가
   const handleAddTodo = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("값 들어오는지 확인", newTitle);
+    console.log("값 들어가는지 확인", newTitle);
 
     const title = newTitle.trim();  // 공백은 지우기
     if(!title) return;
@@ -18,14 +52,31 @@ function App() {
     try{
       await addTodo(title);
       setNewTitle("");
+      const data = await fetchTodos();
+      setTodos(data);
+      console.log("뭐가 들어오냐?", data);
+
     }catch(error){
       console.error(error);
-      alert("추가 중 오류가 발생하였습니다.");
+      alert("追加の途中エラーが発生しました");
     }finally{
       setAdding(false); // 다 끝나면 추가 버튼 원래 상태로 해줘야함
     }
 
   }
+
+  // 삭제
+  const handleDelete = async (id: string) => {
+    if(!confirm("本当に削除しますか？"))　return;
+    try {
+    await deleteTodo(id);
+    setTodos(prev => prev.filter((todo) => todo.id !== id));
+  } catch(error){
+    console.error(error);
+    alert("削除中、エラーが発生しました。");
+  }
+  
+  } 
 
   return (
     <>
@@ -37,14 +88,40 @@ function App() {
             type="text"
             value={newTitle}
             onChange={(e) => setNewTitle(e.target.value)}
-            placeholder="할 일을 입력하세요."
+            placeholder="TODO　LISTを入力してください。"
             className="flex-1 border border-slate-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 "
             />
             <button 
             disabled={adding}  // 값을 넣고 처리되기까지는 비활성화
             className="px-4 py-2 rounded-lg bg-blue-600 text-white font-medium 
-            disabled:opcaity-60 disabled:cursor-not-allowed hover:bg-blue-700 transition">{adding ? '추가 중...' : '추가'}</button>
+            disabled:opcaity-60 disabled:cursor-not-allowed hover:bg-blue-700 transition">{adding ? 'Adding...' : 'Add'}</button>
           </form>
+          {/* 로딩 표시 */}
+          {loading && (
+            <p className="text-center text-slate-500">Loaing...</p>
+          )}
+          {/* Todo 목록 */}
+          {!loading && todos.length === 0 && (
+            <p className="text-center text-slate-400">Todo Listが空いてます。</p>
+          )}
+          <ul className="space-y-2 max-h-[400px] overflow-y-auto">
+            {todos.map((todo) => (
+              <li 
+                key={todo.id}
+              className="flex items-center justify-between bg-slate-50 border border-slate-200 rounded-xl px-3 py-2">
+                <label className="flex items-center gap-2 flex-1">
+                  <input type="checkbox" checked={todo.completed} className="w-4 h-4"　onChange={(e) => handleToggle(todo.id, e.target.checked)} />
+                  <span 
+                    className={`text-sm ${todo.completed ? 'line-through text-slate-400' : 'text-slate-700'}`}
+                  >{todo.title}
+                  </span>
+                </label>
+                <button
+                  onClick={() => {handleDelete(todo.id)}}
+                  className="text-xs text-red-500 hober:text-red-600 px-2 py-1 transition">Delete</button>
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
     </>
--- src/services/todoService.ts
+++ src/services/todoService.ts
@@ -1,7 +1,20 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, orderBy, getDocs, query, doc, updateDoc, deleteDoc } from "firebase/firestore";
 import { db } from "../firebase"; 
+import type { Todo } from "../types/todo";
 
 const todosCollection = collection(db, "todos");
+
+// 모든 Todo 가져오기
+export async function fetchTodos(): Promise<Todo[]> {
+    const q = query(todosCollection, orderBy("createdAt", "desc"));
+    const snapshot = await getDocs(q);
+    return snapshot.docs.map(d => (
+        {
+            id: d.id,
+            ...(d.data() as Omit<Todo, "id">)
+        }
+    ))
+}
 
 export async function addTodo(title: string): Promise<void> {
     // FireStore 저장 부분
@@ -11,4 +24,16 @@ export async function addTodo(title: string): Promise<void> {
         completed: false,
         createdAt: Date.now()
     });
+}
+
+// 완료 상태 코드
+export async function toggleTodo(id: string, completed: boolean){
+    const ref = doc(db, "todos", id);
+    await updateDoc(ref, {completed});
+}
+
+// 삭제 코드
+export async function deleteTodo(id: string){
+    const ref = doc(db, "todos", id);
+    await deleteDoc(ref);
 }
